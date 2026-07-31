139862e57cdcf7757815bb013425315c2383bfd7
amirkhan1092 <amirkhan1092@gmail.com>
2026-08-01 00:06:12 +0530

    Solution update nextround800

--- CodeForces/NextRound800.java
+++ CodeForces/NextRound800.java
@@ -1,16 +1,23 @@
-package CodeForces;
+// package CodeForces;
 
 import java.util.Scanner;
 
 public class NextRound800 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // NextRound800.java
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
         int counter=0;
+        int [] arr = new int[N];
         for(int i=0; i<N; i++){
-            if(sc.nextInt() > K) counter++;
+            arr[i] = sc.nextInt();
         }
+
+        K = arr[K-1];
+        for(int val : arr){
+            if(val >= K && val != 0) counter++;
+        }
+
         sc.close();
 
         System.out.println(counter);
