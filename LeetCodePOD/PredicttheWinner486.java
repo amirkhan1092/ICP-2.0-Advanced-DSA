a1a56f32d4276a3bbb236f6fd47858d859bd057d
amirkhan1092 <amirkhan1092@gmail.com>
2026-08-02 13:54:49 +0530

    Predict the Winner

---
+++ LeetCodePOD/PredicttheWinner486.java
@@ -0,0 +1,31 @@
+package LeetCodePOD;
+
+import java.util.Scanner;
+
+public class PredicttheWinner486 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+    //    int n = sc.nextInt();
+       int n = sc.nextInt();
+       int m = sc.nextInt();
+       int[][] arr = new int[n][m];
+       for (int i = 0; i < n; i++) {
+           for (int j = 0; j < m; j++) {
+               arr[i][j] = sc.nextInt();
+       }
+       }
+
+       int n = sc.nextInt();
+       int n=sc.nextInt();
+       int m=sc.nextInt();
+       int[][] mat=new int[n][m];
+       for(int i=0;i<n;i++){
+         for(int j=0;j<m;j++){
+           mat[i][j]=sc.nextInt();
+         }
+       }
+
+
+       }
+    }
+
